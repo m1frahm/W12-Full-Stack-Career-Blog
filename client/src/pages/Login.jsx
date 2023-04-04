ce62945f6c322d9aa40579dba5115ab627385358
Mifrah Malik <mifrah.malik@gmail.com>
2023-04-04 14:38:37 -0400

    laid out front-end pages and added basic functions

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+//can use rafc shortcut
+
+const Login = () => {
+  return (
+  <div>Login</div>
+  )};
+
+export default Login;
---
+++ client/src/pages/WriteOrAdd.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const WriteOrAdd = () => {
+    return (
+        <div>Write or Add Article Page</div>
+    )
+}
+
+export default WriteOrAdd
