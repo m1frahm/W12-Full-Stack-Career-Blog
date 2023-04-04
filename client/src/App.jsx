eb1120bf9c56b89b418263c1e96d77d95dcc0a9e
Mifrah Malik <mifrah.malik@gmail.com>
2023-04-04 18:20:42 -0400

    followed a diff approach to create routes using this link for ref https://github.com/remix-run/react-router/blob/dev/examples/basic/src/main.tsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,49 +1,40 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Outlet,
-} from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
 import "./index.css";
-import Single from './pages/Single';
-import Login from './pages/Login';
-import Write from './pages/Write';
-import Home from './pages/Home';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+import Single from "./pages/Single";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
-const Layout = () => {
-  return (
-    <>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
-    </>
-  )
-}
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello</div>
-  },
-  {
-    path: "/login",
-    element: <Login/>
-  },
-]);
+// const Layout = () => {
+//   return (
+//     <>
+//       <Navbar/>
+//       <Outlet/>
+//       <Footer/>
+//     </>
+//   )
+// }
 
 function App() {
-
   return (
-    <div>
-  <RouterProvider router={router}/> 
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Navbar />
+            <Home />
+            <Footer />
+          </>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
-
-
-export default App
+export default App;
