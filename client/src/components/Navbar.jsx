8cccc877c937d55c332be5603ebd130ebb72cb5d
Mifrah Malik <mifrah.malik@gmail.com>
2023-04-05 15:12:18 -0400

    adding divs in navbar.jsx to be used for styling later

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,6 @@
 // import Nav from 'react-bootstrap/Nav';
 // import Logo from '../assets/BlueTechtonicaWord.png'
 
-
 // function MyNavBar(props) {
 
 //   return (
@@ -33,14 +32,17 @@
 
 // export default MyNavBar;
 
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">logo</div>
+        <div className="links">links</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
