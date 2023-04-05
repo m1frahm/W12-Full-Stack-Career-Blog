c3764fc8baffea0a4a07f40a83a003bf627853ca
Mifrah Malik <mifrah.malik@gmail.com>
2023-04-05 14:31:30 -0400

    playing around with the position of my app and container div

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,37 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, Outlet } from "react-router-dom";
-import "./index.css";
+import "./index.css"; //this one might not be necessary
 import Single from "./pages/Single";
 import Login from "./pages/Login";
 import Write from "./pages/Write";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import "./style.scss";
 
 function App() {
   return (
-    <div className="app">
-      <div className="container">
-        <Routes>
-          <Route
-            element={
-              <>
-                <Navbar />
+    <Routes>
+      <Route
+        element={
+          <>
+            <Navbar />
+            <div className="app">
+              <div className="container">
                 <Outlet />
-                <Footer />
-              </>
-            }
-          >
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/write" element={<Write />} />
-            <Route path="/post/:id" element={<Single />} />
-          </Route>
-        </Routes>
-      </div>
-    </div>
+              </div>
+            </div>
+            <Footer />
+          </>
+        }
+      >
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/post/:id" element={<Single />} />
+      </Route>
+    </Routes>
   );
 }
 
