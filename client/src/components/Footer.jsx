16d8f7cb12464cf0ee9cdc4fff61e064913794f0
Mifrah Malik <mifrah.malik@gmail.com>
2023-04-05 16:21:40 -0400

    working on making adjustments to footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Logo from "../images/intelligent_change_logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div>
+    <img src={Logo} alt="" />
+    <span> 💛 A boost of inspiration 💛</span>
+  </div>;
 };
 
 export default Footer;
