e9f6a973572836649ad48543ffd1c9750846df3c
Mifrah Malik <mifrah.malik@gmail.com>
2023-04-05 14:26:35 -0400

    adding login button

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,6 +8,7 @@ const Login = () => {
       <form>
         <input type="text" placeholder='username'></input>
         <input type="password" placeholder='password'></input>
+        <button>Login</button>
       </form>
     </div>
   );
