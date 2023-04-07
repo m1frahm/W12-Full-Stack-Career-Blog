a74714c28db6d85e97fc207e13749f33346567fd
Mifrah Malik <mifrah.malik@gmail.com>
2023-04-07 14:17:19 -0400

    starting to add my first usestate hook

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
-const Write = () => {
-    return (
-        <div className="add">
-            <div className="content">
-            <input type="text" placeholder="Title of Article" />
-            </div>
-            <div className="menu">
-            <div className="item">item1</div>
-            <div className="item">item2</div>
-            </div>
+let Write = () => {
 
+    let [value, setValue] = useState('')
+    console.log(value);
+  return (
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title of Article" />
+        <div className="editorContainer">
+          <ReactQuill
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
         </div>
-    )
-}
+      </div>
+      <div className="menu">
+        <div className="item">item1</div>
+        <div className="item">item2</div>
+      </div>
+    </div>
+  );
+};
 
-export default Write
+export default Write;
